cb0663b1030dbc3f24bf20c6934425c69e0b413e
dip roy <64752174+dipto-roy@users.noreply.github.com>
2023-01-29 23:42:16 +0600

    Add files via upload

---
+++ Lucky_Number.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    BOOST
+
+    //this code use bruteforce:-
+   /*int x;
+   scanf("%i",&x);
+
+   if(x%4==0 || x%7==0 || x%44==0 || x%47==0 || x%74==0 || x%77==0 || x%444==0 || x%447==0 || x%474==0||x%477==0 || x%744==0 || x%774==0 || x%777==0)
+   {
+       printf("YES\n");
+   }
+   else
+   {
+       printf("NO\n");
+   }*/
+
+   //this code use by loop:-
+
+   /*int lucky[]={4,7,47,74,77,444,447,474,477,744,747,777};
+   int count=0;
+   int n;
+   cin>>n;
+
+   for(int i=0; i<12; i++)
+   {
+       if(n%lucky[i]==0) count++;
+   }
+  if(count>0) cout<<"YES"<<endl;
+  else cout<<"NO"<<endl;*/
+
+
+    return 0;
+}
+
+
