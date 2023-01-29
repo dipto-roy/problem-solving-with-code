3cfd852edee003b6a0ec7dcfb24c47e0a2403482
dip roy <64752174+dipto-roy@users.noreply.github.com>
2023-01-29 23:43:50 +0600

    Update Lucky_Number.cpp

--- Lucky_Number.cpp
+++ Lucky_Number.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     BOOST
-
+    //when you use this code must chosses one code and remove comment
     //this code use bruteforce:-
    /*int x;
    scanf("%i",&x);
