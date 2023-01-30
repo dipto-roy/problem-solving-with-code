be9660687eff13da234db49bd8579a3fc7eef964
Dip Roy <64752174+dipto-roy@users.noreply.github.com>
2023-01-31 02:32:38 +0600

    Add files via upload

---
+++ A+B.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+int main()
+{
+    BOOST
+  int t;
+  scanf("%i",&t);
+
+  while(t--)
+  {
+      int a,b;
+      scanf("%i %i",&a,&b);
+      printf("%i\n",a+b);
+  }
+
+
+
+    return 0;
+}
+
+
