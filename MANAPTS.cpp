eee63f6efcca1289e30ac91e0be2d448d7709013
Dip Roy <64752174+dipto-roy@users.noreply.github.com>
2023-01-31 02:42:27 +0600

    Add files via upload

---
+++ MANAPTS.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    if(x<y)
+	    {
+	        cout<<y/x<<endl;
+	    }
+	    else 
+	    {
+	        cout<<0<<endl;
+	    }
+	    
+	    
+	}
+	return 0;
+}
