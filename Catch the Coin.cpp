83987be5736b09d641b2093522560719ee63747a
Dip Roy <64752174+dipto-roy@users.noreply.github.com>
2024-06-29 15:56:32 +0600

    Add files via upload

---
+++ Catch the Coin.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int mx = 1e6 + 123;
+#define endl '\n';
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+int main()
+{
+    BOOST
+
+    int tt;
+    cin>>tt;
+
+    while(tt--)
+    {
+        int x , y;
+        cin>>x>>y;
+
+        if(y >= -1)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+
+        }
+    }
+
+
+
+
+    return 0;
+}
+
+
