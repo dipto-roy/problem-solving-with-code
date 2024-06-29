cd8b3528acbc60cb5a9d6e5c0f4f5eddc000da5a
Dip Roy <64752174+dipto-roy@users.noreply.github.com>
2024-06-30 01:46:20 +0600

    Add files via upload

---
+++ Maximum Multiple Sum.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int mx = 1e6 + 123;
+#define endl '\n';
+#define BOOST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+int main()
+{
+    BOOST
+
+   int tt;
+   cin>>tt;
+
+   while(tt--)
+   {
+       int n;
+       cin>>n;
+       cout<<(n == 3 ? 3:2)<<endl;
+
+
+   }
+
+
+
+
+    return 0;
+}
+
+
+
